5d42dc5d29730164944a35b53eeb457b21745b8d
Naomirenney <naomirenney@gmail.com>
2024-11-20 14:05:40 +0200

    vowel and consonant counter
    added 2 functions

--- Python_Work.py
+++ Python_Work.py
@@ -1,6 +1,5 @@
 
-
-
+#Financial Compound interest calculator function
 def compound_interest():
     starting_balance= float(input("Please enter your initial investment: "))
     interest_rate= float(input("Please enter your interest rate: "))
@@ -17,3 +16,36 @@ def compound_interest():
 #call compund_interest function
 compound_interest()
 
+
+
+#Vowel counting in a string function 
+def count_vowels():
+    sentence= input("Enter a sentence/word and I will count the vowels: ")
+    
+    list= "aeiouAEIOU"
+    
+    count=0
+    
+    for x in sentence:
+        if x in list:
+            count+=1
+    print(f"There are {count} vowels in this sentence/word")
+    return count
+
+
+def count_consonants():
+    sentence= input("Enter a sentence/word and I will count the consonants: ")
+
+    list= "aeiouAEIOU"
+    
+    count=0
+
+    for x in sentence:
+        if not x in list and x.isalpha():
+            count+=1
+    print(f"There are {count} consonants")
+    return count
+
+#Call vowel counter and consonant counter functions
+count_vowels()
+count_consonants()
